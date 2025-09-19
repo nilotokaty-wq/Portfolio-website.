1e5d415594c6f81cdd0dc88b7146eaca146dd6d1
nilotokaty-wq <nilotokaty259@gmail.com>
2025-09-19 20:14:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Download resume
+function downloadResume() {
+  window.open("resume.pdf", "_blank"); // put your resume.pdf in the repo
+}
+
+// Dark mode toggle
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
